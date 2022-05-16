a042cfd17599fc3aa209f68e7a7fcfd81cbd5556
ivantashirev <ivantashirev@yandex.ru>
2022-05-16 17:06:30 +0300

    Homework3

---
+++ src/main/java/lesson3/SeleniumStart.java
@@ -0,0 +1,16 @@
+package lesson3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumStart {
+    public static void main(String[] args) throws InterruptedException  {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
